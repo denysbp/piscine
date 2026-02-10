d4b2e303bd19516166ebae2aa0e6113659a4bd4a
denys Bonfim <denysbonfim1@gmail.com>
2026-02-10 12:46:46 +0000

    C04

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,26 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	resultado;
+	int	signal;
+
+	i = 0;
+	resultado = 0;
+	signal = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signal *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		resultado = resultado * 10 + (str[i] - '0');
+		i++;
+	}
+	return (signal * resultado);
+}
